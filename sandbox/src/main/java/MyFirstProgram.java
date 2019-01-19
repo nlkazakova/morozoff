e9f8f657ce5d4a938f64ccb2881c735a1543abb4
nlkazakova <nlkazakova@users.noreply.github.com>
2019-01-19 21:16:01 +0300

    Первое web-приложение на spring-boot

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,16 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
 public class MyFirstProgram {
-	
-	public static void main (String[] args){
-		System.out.println("Hello, world!");
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProgram.class, args);
+	}
 }
